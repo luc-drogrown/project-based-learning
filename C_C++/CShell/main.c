cdc2fad30b0959b16847dcb257eeec5bf640d08b
Lucian Dragan <draganlucian18@gmail.com>
2024-11-25 12:03:28 +0200

    CShell

---
+++ C_C++/CShell/main.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LSH_RL_BUFSIZE 1024
+
+char *lsh_read_line(void)
+{
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if (!buffer)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+        // Read a character
+        c = getchar();
+
+        // If we hit EOF, replace it with a null character and return.
+        if (c == EOF || c == '\n')
+        {
+            buffer[position] = '\0';
+            return buffer;
+        } else
+        {
+            buffer[position] = c;
+        }
+        position++;
+
+        //if we have exceeded the buffer, reallocate.
+        if (position >= bufsize)
+        {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer)
+            {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+/* newer version of the same function, using the getline command that does
+most of the work
+
+char *lsh_read_line(void)
+{
+  char *line = NULL;
+  ssize_t bufsize = 0; // have getline allocate a buffer for us
+
+  if (getline(&line, &bufsize, stdin) == -1){
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS);  // We recieved an EOF
+    } else  {
+      perror("readline");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  return line;
+}
+*/
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM" \t\r\n\a"
+char **lsh_split_line(char *line)
+{
+    int bufsize = LSH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if (!tokens)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_TOKEN);
+    }
+
+    token = strtok(line, LSH_TOK_DELIM);
+    while (token != NULL)
+    {
+        tokens[position] = token;
+        position++;
+
+
+    }
+
+}
+void lsh_loop(void)9
+{
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
+int main(int argc, char **argv)
+{
+    //load config files
+
+    //run command loop
+
+    lsh_loop;
+    //perform cleanup/shutdown
+
+    return EXIT_SUCCESS;
+    return 0;
+}
